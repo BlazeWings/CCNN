3cac4a939b9aef8dafbd8406fa4c737a5eef8e87
BlazeWings <132177970+BlazeWings@users.noreply.github.com>
2024-03-30 18:09:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, render_template_string,render_template
-from jinja2 import Environment, escape
-from itsdangerous import json
 
 
 app = Flask(__name__)
@@ -8,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
    
-     
+     e=1
     return render_template('index.html',e=e)
 
 if __name__ == '__main__':
