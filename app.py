f665bc5a64dc4c9d33ff779d1df792c19f4522ae
BlazeWings <132177970+BlazeWings@users.noreply.github.com>
2024-03-30 18:57:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, render_template_string,render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-   
-     e=1
-    return render_template('index.html',e=e)
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        result = num1 + num2
+        return render_template('index.html', result=result)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
