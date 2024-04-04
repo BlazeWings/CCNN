8ec4d526257dc87f99f016ff878c7e2aabc0b761
BlazeWings <132177970+BlazeWings@users.noreply.github.com>
2024-04-04 19:46:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def index():
         num2 = float(request.form['num2'])
         result = num1 + num2
         return render_template('index.html', result=result)
+@app.route('/66', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        result = num1 + num2
+        return render_template('dd.html', result=result)        
    
 
 if __name__ == '__main__':
